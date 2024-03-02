1a5436fa75e685b4ed14ef8e731eb4518b44f4b3
Tim Hahn <7631802+timdhahn@users.noreply.github.com>
2024-03-02 09:36:53 -0500

    hide logo

--- components/hero.js
+++ components/hero.js
@@ -59,7 +59,7 @@ const Hero = () => {
           </div>
         </div>
       </Container>
-      <Container>
+      {/* <Container>
         <div className="flex flex-col justify-center">
           <div className="text-xl text-center text-gray-700 dark:text-white">
             Trusted by <span className="text-indigo-600">1+</span>{" "}
@@ -67,16 +67,13 @@ const Hero = () => {
           </div>
 
           <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
-            {/* <div className="pt-2 text-gray-400 dark:text-gray-400">
-              <AmazonLogo />
-            </div> */}
  
             <div className="pt-2 text-gray-400 dark:text-gray-400">
               <PennMedLogo />
             </div>
           </div>
         </div>
-      </Container>
+      </Container> */}
     </>
   );
 }
